f420fb51a1f9d0c0728bdd69e4359c6e5e428f2d
Kristian <klm@myhost.(none)>
2011-09-18 20:57:50 +0200

    corelib: basic library working with is-prime function!

--- corelib.lisp
+++ corelib.lisp
@@ -2,41 +2,106 @@
                                         ; test
                                         ; this should be ignored be our interpreter :S
                                         ; and this too
-(quote (loading corelib))
-
-(define inc
-  (lambda (x)
-    (+ 1 x)))
+(print 'loading 'corelib)
 
 
-;(define fib (lambda (x) 
- ;(if ((= x 0) 1)
-                                       ;                 ((= x 1) 1)
-  ;  (+ (+ (fib (- x 1)) (fib (- x 2))))
-;))
- ;(fib 23))
-;(/ 10 3)
-;(* (/ 10 3) 3)
+(define rest cdr)
 
+(define list-length
+  (lambda (L)
+    (if L
+        (+ 1 (list-length (cdr L)))
+      0)))
+
+
+; I am looking forward to get this working:
+(define rest-test ; that's not rest as in rest services ... but I hope
+                  ; you already knew that
+  (lambda (a &rest b)
+    (begin
+     (print 'a 'is a)
+     (print 'rest 'is &rest)
+     (print 'b 'is b))))
+
+;; (define inc
+;;   (lambda x
+;;     (if x
+;;         ((lambda ()
+;;            (print 'cdr 'is (cdr x))
+;;            (print 'called 'inc 'for 'x '= (cdr  x))
+;;            (cons (+ 1 x) (inc (cdr x)))
+;;            ))
+;;       nil
+;;       )
+;;     )
+;;   )
+;(inc nil)
+;(inc 1 2 3 4 5 6 7 8 9)
+
+
+
+(define factorial
+  (lambda (N)
+    (if (= N 1)
+        1
+      (* N (factorial (- N 1))))))
+
+
+                                        ; remainder (% operator)
+                                        ; returns 0 if x is divisible by d
 (define rem 
   (lambda (x d)
-    (- x (* (/ x d) d))
-    )
-  )
+    (- x (* (/ x d) d))))
 
 (define is-even
   (lambda (x)
     (if (= 0 (rem x 2))
         t
-      nil)
-    )
-  )
+      nil)))
 
 (define is-odd
   (lambda (x)
     (if (is-even x)
         nil
-      t)
-    )
-  )
+      t)))
+
+(define is-divisible
+  (lambda (x y)
+    (if (= y 1)
+        nil
+      (if (>= y x)
+          nil
+        (if (= 0 (rem x y))
+            t
+          nil
+          )))))
+
+(define is-prime
+  (lambda (x)
+    (if (is-even x)
+        nil
+      (is-prime-rec x 1))))
+
+(define is-prime-rec
+  (lambda (x y)
+    (if (is-divisible x y)
+        nil
+      (if (>= y x)
+          t
+        (is-prime-rec x (+ 2 y))))))
+
+(define is-prime-on-list
+  (lambda (x)
+    (if x
+        (begin
+         (print 'prime-check (car x) (is-prime (car x) 1))
+         (is-prime-on-list (rest x)))
+      nil)))
+
+
+(is-prime-on-list '(1 2 3 4 5 6 7 8 9 10 11 12 13 103))
+
+
+
+
 
